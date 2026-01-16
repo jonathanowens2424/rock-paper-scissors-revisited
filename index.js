31ea78d9feeb1d5b6232ac4e12b13a119e782e64
Jonathan <jonathanlowens@live.com>
2026-01-16 08:53:48 -0600

    Added UI elements and fixed error with Ties not updating computer choice

--- index.js
+++ index.js
@@ -12,45 +12,54 @@ function getComputerChoice() {
   }
 }
 
-function checkIfTie(computerChoice, userChoice) {
+function determineResults(computerChoice, userChoice) {
   if (computerChoice === userChoice) {
     console.log("Tie");
-    return getUserChoice();
+    return;
   }
 
-  //if the user picked rock
   if (userChoice === "rock") {
     if (computerChoice === "paper") {
-      console.log(`You picked rock. The computer picked paper. You lost!`);
+      console.log("You picked rock. The computer picked paper. You lost!");
     } else if (computerChoice === "scissors") {
-      console.log(`You picked rock. The computer picked scissors. You won!`);
+      console.log("You picked rock. The computer picked scissors. You won!");
     }
+    return;
   }
-
-  // if the user picked paper
 
   if (userChoice === "paper") {
     if (computerChoice === "scissors") {
-      console.log(`You picked paper. The computer picked scissors. You lost!`);
+      console.log("You picked paper. The computer picked scissors. You lost!");
     } else if (computerChoice === "rock") {
-      console.log(`You picked paper. The computer picked rock. You won!`);
+      console.log("You picked paper. The computer picked rock. You won!");
     }
+    return;
   }
 
-  // if the user picked scissors
   if (userChoice === "scissors") {
     if (computerChoice === "rock") {
-      console.log(`You picked scissors. The computer picked rock. You lost!`);
+      console.log("You picked scissors. The computer picked rock. You lost!");
     } else if (computerChoice === "paper") {
-      console.log(`You picked scissors. The computer picked paper. You won!`);
+      console.log("You picked scissors. The computer picked paper. You won!");
     }
+    return;
   }
 }
-
-let computerChoice = getComputerChoice();
-
 const paperButton = document.querySelector("#paper");
 const rockButton = document.querySelector("#rock");
 const scissorsButton = document.querySelector("#scissors");
 
-checkIfTie(computerChoice, userChoice);
+paperButton.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+  determineResults(computerChoice, "paper");
+});
+
+rockButton.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+  determineResults(computerChoice, "rock");
+});
+
+scissorsButton.addEventListener("click", () => {
+  const computerChoice = getComputerChoice();
+  determineResults(computerChoice, "scissors");
+});
